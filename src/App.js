364f060e47f9d2162f1dcfd8091e6b46ccd2c8df
ManojGurung2002 <58944421+ManojGurung2002@users.noreply.github.com>
2022-09-16 20:36:53 -0500

    Add files via upload
    This is the part where we've implemented Search box and dynamically changing the Movie List by using the useState to track the change in Search Box and making the Fetch request in accordance...

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 
 const App = () => {
@@ -11,31 +12,33 @@ const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
-  const getMovieRequest = async () => {
-  const url = "http://www.omdbapi.com/?s=avengers&apikey=8839a974";
+  const getMovieRequest = async (searchValue) => {
+  const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=8839a974`;
   
   const response = await fetch(url);
   const responseJson = await response.json();
 
-  console.log(responseJson);
-  setMovies(responseJson.Search);
+  if (responseJson.Search) {
+    setMovies(responseJson.Search);
+  }
+  
   };
 
   useEffect (()=>{
-    getMovieRequest();
-  }, []);
+    getMovieRequest(searchValue);
+  }, [searchValue]);
   return (
 
     <div className="container-fluid movie-app">
-      <div className='row'>
-        <MovieListHeading heading='Movies' />
+      <div className='row d-flex align-items-center mt-3 mb-3'>
+        <MovieListHeading heading='Movies'/>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
       </div>
-
       <div className="row">
       <MovieList movies={movies}/>
       </div>  
-      
-       
+
+
     </div>
 
   );
