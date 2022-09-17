b200b6c94e9ff7ff71c19d9f496dacdaadbbe1ce
ManojGurung2002 <58944421+ManojGurung2002@users.noreply.github.com>
2022-09-16 20:03:26 -0500

    Add files via upload
    This is the project up to calling fetch request and getting the movie list; Displaying the received movies in a row which is scrollable;

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import './App.css'
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+
+
+const App = () => {
+
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const getMovieRequest = async () => {
+  const url = "http://www.omdbapi.com/?s=avengers&apikey=8839a974";
+  
+  const response = await fetch(url);
+  const responseJson = await response.json();
+
+  console.log(responseJson);
+  setMovies(responseJson.Search);
+  };
+
+  useEffect (()=>{
+    getMovieRequest();
+  }, []);
+  return (
+
+    <div className="container-fluid movie-app">
+      <div className='row'>
+        <MovieListHeading heading='Movies' />
+      </div>
+
+      <div className="row">
+      <MovieList movies={movies}/>
+      </div>  
+      
+       
+    </div>
+
+  );
+};
+
+export default App;
